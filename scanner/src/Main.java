d15281daa48e0eb1ee9a7f58af5c15d9a0a19bfe
unknown <samudrte2004@gmail.com>
2025-05-16 20:14:27 -0600

    Ultima Actualizacion

---
+++ scanner/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner (System.in);
+       System.out.print("ingresa una edad:\n");
+       
+        int edad = Sc.nextInt();
+        System.out.print(edad);
+
+    }
+}
